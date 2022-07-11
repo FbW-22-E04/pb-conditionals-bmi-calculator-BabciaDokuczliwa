080199a2c09d16ed1a31c771e0b0a059a7a5fc9b
BabciaDokuczliwa <oblaczynska@gmail.com>
2022-07-11 14:50:55 +0200

    ex with conditionals -bmi

--- solution.js
+++ solution.js
@@ -16,3 +16,27 @@
 
 //Q5 Create an *if* statement which prints the name and BMI of the person with the highest BMI. 
 
+const markHeight = 1.75;
+const markMass = 70.2;
+const johnHeight = 1.84;
+const johnMass = 81.3;
+
+
+const bmiMark = markMass / (markHeight * markHeight);
+const bmiJohn = johnMass / (johnHeight * johnHeight);
+
+console.log('Mark BMI is ', bmiMark, 'John`s BMI is ', bmiJohn);
+
+const markMoreBeefCake = bmiMark > bmiJohn;
+console.log(markMoreBeefCake);
+
+
+console.log('Q4: Is Mark`s BMI higher than John`s?', markMoreBeefCake ?  'Yes, it is' : 'No');
+
+if (bmiMark > bmiJohn) {
+    console.log('Q5. Mark`s BMI is higher than John`s and is ', Math.round( bmiMark * 100) / 100);
+    
+} else {console.log('Q5. John`s BMI is higher than Mark`s and is ', Math.round( bmiJohn * 100) / 100);
+
+
+}
